bdd42cf0c4e379ea080a09c154394a864a8f42f0
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2026-09-21 10:11:18 +0530

    fix db deployment issue

--- terraform/postgres.tf
+++ terraform/postgres.tf
@@ -8,8 +8,9 @@ resource "azurerm_postgresql_flexible_server" "main" {
   sku_name   = "GP_Standard_D2s_v3"
   storage_mb = 32768
 
-  administrator_login    = var.db_admin_username
-  administrator_password = var.db_admin_password
+  administrator_login           = var.db_admin_username
+  administrator_password        = var.db_admin_password
+  public_network_access_enabled = false
 
   delegated_subnet_id = azurerm_subnet.db.id
   private_dns_zone_id = azurerm_private_dns_zone.zones["postgres"].id
